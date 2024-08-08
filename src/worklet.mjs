b5092e15f8b2b61c8f74a40e3d7e48ff1bcfec19
Pedro Miranda <pedromiramor@gmail.com>
2024-08-08 00:08:04 -0300

    Remove legacy files from abracm/ledmat
    Starting over fresh

--- src/worklet.mjs
+++
@@ -1,7 +0,0 @@
-export class LedmatWorklet extends AudioWorkletProcessor {
-	process(inputs, _outputs, _parameters) {
-		console.log({ inputs });
-	}
-}
-
-registerProcessor('LedmatWorklet', LedmatWorklet);
